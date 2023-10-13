298929dacf5ba3cc152ed4765319cc64d3d9f32b
pindorama <pindorama@users.noreply.github.com>
2023-10-13 10:33:46 +0100

    feat: added react-route-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,9 @@
-export{};
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => {
+  return (<Routes>
+    <Route path='/start-page' element={<p> start page </p>}/>
+    <Route path='*' element={<Navigate to='/start-page'/>}/>
+  </Routes>
+  );
+};
