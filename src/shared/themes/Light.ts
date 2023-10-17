17aa21b78734b535865f0fa15469ad26ce6a6713
pindorama <pindorama@users.noreply.github.com>
2023-10-17 15:25:54 +0100

    feat: added Thema

--- src/shared/themes/Light.ts
+++ src/shared/themes/Light.ts
@@ -1,1 +1,27 @@
-import {createTheme }   from '@mui/material';
+import { createTheme }   from '@mui/material';
+import { cyan } from '@mui/material/colors';
+import yellow from '@mui/material/colors/yellow';
+
+
+export const LightTheme = createTheme({
+  palette: {
+    primary:{
+      main: yellow[700],
+      dark: yellow[700],
+      light: yellow[700],
+      contrastText:'#ffffff',
+    },
+        
+    secondary: {
+      main: cyan[700],
+      dark: cyan[700],
+      light: cyan[700],
+      contrastText:'#ffffff',
+
+    },
+    background: {
+      paper: '#ffffff',
+      default:'#f7f6f3'
+    }
+  }
+});
